4aa151a10c558b7dc3f217a82d07b141f5d0f787
mayurgite.it@gmail.com <mayurgite.it@gmail.com>
2024-02-04 15:45:48 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function App() {
+  const [countries, Setcountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => Setcountries(data));
+  }, []);
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    padding: "10px",
+    margin: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  };
+  return (
+    <div style={containerStyle}>
+      {countries.map((country) => (
+        <div key={country.cca3} style={cardStyle}>
+          <img
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            style={imageStyle}
+          />
+          <h2>{country.name.common}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
