9f6151b2cc2cd390b278bbb82ce721fb6a214e26
mayurgite.it@gmail.com <mayurgite.it@gmail.com>
2024-02-06 19:19:22 +0530

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ export default function App() {
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => Setcountries(data));
+      .then((data) => Setcountries(data))
+      .catch((err)=>console.error("Error fetching data: " , err))
   }, []);
   const cardStyle = {
     width: "200px",
